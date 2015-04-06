a07e4017f6d2bc7850b9d37ce28c050aab3dd6f1
yburyug <bobbygrayson@gmail.com>
2015-04-06 14:21:11 -0400

    Update index.js

--- 4/js/index.js
+++ 4/js/index.js
@@ -1,7 +1,7 @@
 $('body')
 .on('click', '.vimeo-launch', function () {
   var videoID = '121500937',
-      $video = '<div id="vimeo-pop-container"><div><span class="vimeo-close"></span><iframe src="https://www.youtube.com/embed/2Bv80tA3hm8?autoplay=1" width="90%" height="90%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div></div>';
+      $video = '<div id="vimeo-pop-container"><div><span class="vimeo-close"></span><iframe src="http://player.vimeo.com/external/121500937.m3u8?p=high,standard&s=1048cf328dec15e0890dda75480f0cf3&autoplay=1" width="90%" height="90%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div></div>';
   $('body').append($video);
 } )
 
